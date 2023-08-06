46ea4b97330169b019924fe1935f351c98064684
Julius Mercz <137562922+jmercz@users.noreply.github.com>
2023-08-06 23:23:44 +0200

    introduced isomeric state to Isotope docstrings in isotope.py

--- src/isovec/isotope.py
+++ src/isovec/isotope.py
@@ -1,4 +1,7 @@
-# Python class for isotope
+"""Class for Isotope
+
+Isotope class serves as constituent for Element class.
+"""
 
 from __future__ import annotations
 
@@ -6,16 +9,37 @@ from .constants import ATOM_NUMB_TO_SYMBOL, M_u
 
 
 class Isotope:
+    """Isotope are nuclides of the same element.
 
-    def __init__(self, Z: int, A: int, A_r: float, name: str = "") -> None:
+    Each isotope is characterised by their atomic number (Z), mass number (M)
+    and isomeric state (I). Furthermore, each isotope has a chracteristic 
+    relative atomic mass (A_R). They are used to compose elements.
+    """
+
+    def __init__(self, Z: int, A: int, A_r: float, I: int = 0, name: str = "") -> None:
+        """Constructor of isotope.
+        
+        Args:
+            Z:
+                Atomic number (number of protons).
+            A:
+                Mass number (number of protons + neutrons).
+            A_r:
+                Relative atomic mass ("atomic weight") [-].
+            I:
+                Isomeric state.
+            name:
+                Override name of isotope.
+        """
         
         self._Z: int = Z        # atomic number (protons)
         self._A: int = A        # mass number (protons + neutrons)
         self._A_r: float = A_r  # relative atomic mass (atomic weight) [-]
+        self._I: int = I        # isomeric state (0 = ground state, 1 = metastable, ...)
         self._name: str         # name
 
         if name == "":
-            self._name = ATOM_NUMB_TO_SYMBOL[self._Z] + "-" + str(self._A)
+            self._name = self.short_name()
         else:
             self._name = name
 
@@ -45,6 +69,11 @@ class Isotope:
         return self._A_r
 
     @property
+    def I(self):
+        """Isomeric state."""
+        return self._I
+
+    @property
     def M(self):
         """Molar mass [g mol^-1] of the isotope."""
         return self.calc_M()
@@ -56,8 +85,13 @@ class Isotope:
     
     @property
     def ZA(self):
-        """ZA notation of the isotope with atomic and three digit mass number."""
+        """ZA notation of the isotope."""
         return self.ZA_notation()
+    
+    @property
+    def ZAI(self):
+        """ZAI notation of the isotope."""
+        return self.ZAI_notation()
 
 
     # ########
@@ -72,19 +106,54 @@ class Isotope:
         """Calculates neutron number (number of neutrons)."""
         return self._A - self._Z
     
+    def get_symbol(self) -> str:
+        """Returns symbol of element that isotope represents."""
+        return ATOM_NUMB_TO_SYMBOL[self._Z]
+    
     def ZA_notation(self) -> int:
         """Returns the ZA notation of the isotope.
         
-        The mass number is written as a 3 digit number.
+        ZA = Z*1000 + A
         """
-        return int(f"{self._Z}{self._A:03}")
+        return self._Z*1000 + self._A
+    
+    def ZAI_notation(self) -> int:
+        """Returns the ZAI notation of the isotope.
 
+        ZAI = Z*10000 + A*10 + I
+        """
+        return self._Z*10000 + self._A*10 + self._I
+
+    def short_name(self, verbose: bool = False) -> str:
+        """Returns short name of isotope as string.
+        
+        Similar to pronunciation of AZE notation.
+
+        Args:
+            verbose:
+                Flag to explicitly show isomeric state of isotope.
+        """
+        
+        name = f"{self.get_symbol()}-{self._A}"
+        
+        if verbose:
+            if self._I == 0:
+                name += "g"
+            else:
+                name += f"m{self._I}"
+        else:
+            if self._I == 1:
+                name += "m"
+            if self._I > 1:
+                name += f"m{self._I}"
+            
+        return name
+    
 
     # ########
     # Print
     # ########
 
-    
 
 
     # ########
@@ -98,11 +167,11 @@ class Isotope:
         return f"{self.__class__.__name__} \"{self._name}\""
 
     def __hash__(self):
-        return hash((self._Z, self._A))
+        return hash((self._Z, self._A, self._I))
 
     def __eq__(self, other):
         try:
-            if (self._Z == other._Z) and (self._A == other._A):
+            if (self._Z == other._Z) and (self._A == other._A) and (self._I == other._I):
                 return True
             else:
                 return False
@@ -119,6 +188,11 @@ class Isotope:
             elif self._Z == other._Z:
                 if self._A < other._A:
                     return True
+                elif self._A == other._A:
+                    if self._I < other._I:
+                        return True
+                    else:
+                        return False
                 else:
                     return False
             else:
@@ -137,6 +211,11 @@ class Isotope:
             elif self._Z == other._Z:
                 if self._A > other._A:
                     return True
+                elif self._A == other._A:
+                    if self._I > other._I:
+                        return True
+                    else:
+                        return False
                 else:
                     return False
             else:
