06465185ffcf888721ca84391392b9bebca62c85
Julius Mercz <137562922+jmercz@users.noreply.github.com>
2023-08-17 17:04:17 +0200

    started updating changelog

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from src import isovec as iso
 # TODO: copy function
 # TODO (far): IAEA Livechart Data Download API class
 
-
+# TODO: finalise changelog
+# TODO: decide for atomic, weight, volume in Node
+# TODO: change _constituents to _composition etc.
 
 pass
