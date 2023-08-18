9b87a0a382bc485c0953be33d4f95cc34336888d
Julius Mercz <137562922+jmercz@users.noreply.github.com>
2023-08-18 10:29:50 +0200

    removed todos from test.py

--- test.py
+++ test.py
@@ -4,8 +4,4 @@ from src import isovec as iso
 # TODO: copy function
 # TODO (far): IAEA Livechart Data Download API class
 
-# TODO: finalise changelog
-# TODO: decide for atomic, weight, volume in Node
-# TODO: change _constituents to _composition etc.
-
 pass
